707188e763b334ba2953cb00a2e0482556811fc0
Fuad71 <fuadul.hasan@abbvie.com>
2024-11-29 20:05:49 +0600

    for loops in golan

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	mySlice := []string{"dog", "cat", "horse", "banana"}
+	fmt.Println(mySlice)
+
+	for _, value := range mySlice {
+		fmt.Println(value)
+	}
 }
