30fa647b3c262e398428ca75e34233f52a84c2d4
Fuad71 <fuadul.hasan@abbvie.com>
2024-11-29 20:17:14 +0600

    Interface is very interesting

--- main.go
+++ main.go
@@ -1,5 +1,53 @@
 package main
 
-func main() {
+import "fmt"
 
+type Animal interface {
+	Says() string
+	NumberofLegs() int
+}
+
+type Dog struct {
+	name  string
+	Breed string
+}
+
+type Gorilla struct {
+	name          string
+	Color         string
+	NumberOfTeeth int
+}
+
+func main() {
+	dog := Dog{
+		name:  "Puppy",
+		Breed: "Puppet",
+	}
+	PringInfo(dog)
+
+	gorilla := Gorilla{
+		name:          "Gorilla",
+		Color:         "black",
+		NumberOfTeeth: 2,
+	}
+
+	PringInfo(gorilla)
+}
+
+func (g Gorilla) Says() string {
+	return "Groooooog"
+}
+func (g Gorilla) NumberofLegs() int {
+	return 2
+}
+func (d Dog) Says() string {
+	return "woof"
+}
+
+func (d Dog) NumberofLegs() int {
+	return 4
+}
+
+func PringInfo(a Animal) {
+	fmt.Println(a.Says(), a.NumberofLegs())
 }
