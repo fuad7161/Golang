4ccb751b077b45af5cbd81f15995a679826662a7
Fuad71 <fuadul.hasan@abbvie.com>
2024-11-29 18:51:43 +0600

    Variable and Function added

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var firstName, lastName string
+	firstName, lastName = saySomething("fuadul", "hasan")
+	fmt.Printf("%s %s", firstName, lastName)
+}
+
+func saySomething(s, ss string) (string, string) {
+	return s, ss
 }
