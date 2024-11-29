78780e5a5b86956ef96aace31768939bcdf2a0aa
Fuad71 <fuadul.hasan@abbvie.com>
2024-11-29 19:04:04 +0600

    Pointers added

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var myString = "Hello World"
+	fmt.Println(myString)
+	changeUsingPointer(&myString)
+	fmt.Println(myString)
+}
+
+func changeUsingPointer(s *string) {
+	newValue := "Hi from Fuad"
+	*s = newValue
 }
