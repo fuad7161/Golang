a3d667b59ac5a5191f4bae5a74b8cb9fde8f80e6
Fuad71 <fuadul.hasan@abbvie.com>
2024-12-02 13:11:30 +0600

    home and about webpage completed

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+var portNumber = ":8080"
+
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is home page")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := AddValues(2, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("this a the about page ad 2+2 is %d", sum))
+}
+
+// AddValues add two value
+func AddValues(x, y int) int {
+	return x + y
+}
+
 func main() {
+
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
 
 }
