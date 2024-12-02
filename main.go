43e8d95126ea238a1ec80af1d5541a19dc78dea5
Fuad71 <fuadul.hasan@abbvie.com>
2024-12-02 13:25:58 +0600

    Error handling in web api

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -18,15 +19,34 @@ func About(w http.ResponseWriter, r *http.Request) {
 	_, _ = fmt.Fprintf(w, fmt.Sprintf("this a the about page ad 2+2 is %d", sum))
 }
 
+// Divide is the handler for two value
+func Divide(w http.ResponseWriter, r *http.Request) {
+	f, err := DivideValues(0.0, 0.0)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot Divide by 0")
+		return
+	}
+	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 10.0, f))
+}
+
 // AddValues add two value
 func AddValues(x, y int) int {
 	return x + y
 }
 
+// Divide two value
+func DivideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("Cannot divide by zero")
+		return 0, err
+	}
+	return x / y, nil
+}
 func main() {
 
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
 
