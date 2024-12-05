e0f3685ddca4f2cbefae2bff7a61f08b93fd59b8
Fuad71 <fuadul.hasan@abbvie.com>
2024-12-05 17:52:44 +0600

    page form validation done

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -1,8 +1,11 @@
 package forms
 
 import (
+	"fmt"
+	"github.com/asaskevich/govalidator"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 // Form create a custom form struct
@@ -24,12 +27,36 @@ func New(data url.Values) *Form {
 	}
 }
 
+func (f *Form) Required(fields ...string) {
+	for _, field := range fields {
+		value := f.Get(field)
+		if strings.TrimSpace(value) == "" {
+			f.Errors.Add(field, "This field is required")
+		}
+	}
+}
+
 // Has checks if form field is in post and not empty
 func (f *Form) Has(field string, r *http.Request) bool {
 	x := r.Form.Get(field)
 	if x == "" {
-		f.Errors.Add(field, "This field cannot be blank")
 		return false
 	}
 	return true
+}
+
+// MinLength check for string minimum length
+func (f *Form) MinLength(field string, length int, r *http.Request) bool {
+	x := r.Form.Get(field)
+	if len(x) < length {
+		f.Errors.Add(field, fmt.Sprint("This field must be at least %s characters long", length))
+		return false
+	}
+	return true
+}
+
+func (f *Form) IsEmail(field string) {
+	if !govalidator.IsEmail(f.Get(field)) {
+		f.Errors.Add(field, "Invalid email address")
+	}
 }
