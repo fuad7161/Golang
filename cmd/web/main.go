2070ec19dd9a48d4521590253c58ca32ca885c9a
Fuad71 <fuadul.hasan@abbvie.com>
2024-12-09 19:03:10 +0600

    database connected

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,22 +6,47 @@ import (
 	"github.com/alexedwards/scs/v2"
 	"github.com/fuad7161/Golang/tree/Project/Bookings/internal/config"
 	"github.com/fuad7161/Golang/tree/Project/Bookings/internal/handlers"
+	"github.com/fuad7161/Golang/tree/Project/Bookings/internal/helpers"
 	"github.com/fuad7161/Golang/tree/Project/Bookings/internal/models"
 	"github.com/fuad7161/Golang/tree/Project/Bookings/internal/render"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
 var app config.AppConfig
 var portNumber = ":8080"
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 func main() {
+
+	err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
+
+func run() error {
 	// what am I going to put in the session
 	gob.Register(models.Reservation{})
 	// Change this to true when in production
 	app.InProduction = false
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
 
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
@@ -33,6 +58,8 @@ func main() {
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("Cannot create template cache")
+		return err
+
 	}
 	app.TemplateCache = tc
 	app.UseCache = false
@@ -40,12 +67,8 @@ func main() {
 	repo := handlers.NewRepo(&app)
 	handlers.NewHandlers(repo)
 	render.NewTemplate(&app)
-	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	helpers.NewHelpers(&app)
 
-	srv := &http.Server{
-		Addr:    portNumber,
-		Handler: routes(&app),
-	}
-	err = srv.ListenAndServe()
-	log.Fatal(err)
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	return nil
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 	"github.com/fuad7161/Golang/tree/Project/Bookings/internal/config"
 	"github.com/fuad7161/Golang/tree/Project/Bookings/internal/forms"
+	"github.com/fuad7161/Golang/tree/Project/Bookings/internal/helpers"
 	"github.com/fuad7161/Golang/tree/Project/Bookings/internal/models"
 	"github.com/fuad7161/Golang/tree/Project/Bookings/internal/render"
-	"log"
 	"net/http"
 )
 
@@ -68,7 +68,7 @@ func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
 func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 	if err != nil {
-		log.Println(err)
+		helpers.ServerError(w, err)
 		return
 	}
 
@@ -98,6 +98,22 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/reservation-summary", http.StatusSeeOther)
 }
 
+func (m *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request) {
+	reservation, ok := m.App.Session.Get(r.Context(), "reservation").(models.Reservation)
+	if !ok {
+		m.App.ErrorLog.Println("Can't get error from session")
+		m.App.Session.Put(r.Context(), "error", "Can't get reservation form session")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+	m.App.Session.Remove(r.Context(), "reservation")
+	data := make(map[string]interface{})
+	data["reservation"] = reservation
+	render.RenderTemplate(w, r, "reservation-summary.page.tmpl", &models.TemplateData{
+		Data: data,
+	})
+}
+
 // Generals renders the room page
 func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, r, "generals.page.tmpl", &models.TemplateData{})
@@ -120,11 +136,6 @@ func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(fmt.Sprintf("Posted to search availability start date: %s and end date is %s", start, end)))
 }
 
-type jsonResponse struct {
-	OK      bool   `json:"ok"`
-	Message string `json:"message"`
-}
-
 // AvailabilityJSON handle request for availability and send JSON response
 func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
 	resp := jsonResponse{
@@ -133,18 +144,20 @@ func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
 	}
 	out, err := json.MarshalIndent(resp, "", "    ")
 	if err != nil {
-		log.Println(err)
+		helpers.ServerError(w, err)
+		return
 	}
 	//fmt.Println(string(out))
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(out)
 }
 
+type jsonResponse struct {
+	OK      bool   `json:"ok"`
+	Message string `json:"message"`
+}
+
 // Contact renders the availability of info
 func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, r, "contact.page.tmpl", &models.TemplateData{})
-}
-
-func (m *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r, "reservation-summary.page.tmpl", &models.TemplateData{})
 }
