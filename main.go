caa7378975ef6b865398d8ed56c744ca7600f612
fuadul <fuadul202@gmail.com>
2024-12-15 19:27:48 +0600

    basic string

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
 func main() {
+	const s = "สวัสดี"
+
+	fmt.Println(len(s))
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%x ", s[i])
+	}
+	fmt.Println()
+	fmt.Println("Rune count:", utf8.RuneCountInString(s))
+
+	for idx, runeValue := range s {
+		fmt.Printf("%#U starts at %d\n", runeValue, idx)
+	}
 
 }
